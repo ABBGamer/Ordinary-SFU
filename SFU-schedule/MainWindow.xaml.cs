b2240a27e6508c5d4059be09751e15ba6286eb9c
ABBGamer <antongrebnev33@gmail.com>
2022-03-25 15:02:33 +0700

    Добавьте файлы проекта.

---
+++ SFU-schedule/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFU_schedule
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Dec_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://dec.sfu-kras.ru/cabinet/login.zul");
+            //MessageBox.Show("Первая кнопка");
+        }
+
+        private void Ecourse_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://e.sfu-kras.ru/");
+        }
+    }
+}
